6409a21a9a715449e4e713e9e3396c8719ec28c8
h11 <nirisix@gmail.com>
2020-03-11 16:47:51 +0800

    rollbar

---
+++ rollbar/index.js
@@ -0,0 +1,48 @@
+// const TELEGRAM_BOT_TOKEN = ''
+// const TELEGRAM_CHAT_ID = ''
+
+addEventListener('fetch', event => {
+    event.respondWith(handle(event.request))
+})
+
+async function handle(request) {
+    if (request.method.toUpperCase() !== 'POST') {
+        return new Response('Method Not Allowed', { status: 405 })
+    } else {
+        const payload = request.json()
+        dispatchRollbar(payload)
+        return new Response('', { status: 204 })
+    }
+}
+
+async function dispatchRollba(payload) {
+    const evt = payload.event_name
+    if (evt === 'occurrence') {
+        await handleOccurrence(payload.data)
+    }
+}
+
+async function handleOccurrence(data) {
+    const msg = {
+        rollbar: data.url,
+        feedurl: data.occurrence?.feedurl,
+        exception: data.occurrence?.body?.trace_chain?.[0]?.exception?.message,
+    }
+    const text = JSON.stringify(msg, null, 4)
+    await sendToTelegram(text)
+}
+
+async function sendToTelegram(text) {
+    const url = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`
+    const resp = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            chat_id: Number(TELEGRAM_CHAT_ID),
+            text,
+        }),
+    })
+    return resp
+}
