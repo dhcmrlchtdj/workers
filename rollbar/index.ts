9ddffae4a91c5268988ed31fafed6b85d039fda5
h11 <nirisix@gmail.com>
2020-03-15 17:13:15 +0800

    ts

--- rollbar/index.ts
+++ rollbar/index.ts
@@ -1,11 +1,40 @@
-// const TELEGRAM_BOT_TOKEN = ''
-// const TELEGRAM_CHAT_ID = ''
+import {} from '@cloudflare/workers-types'
+
+// from https://docs.rollbar.com/docs/webhooks
+// https://rollbar.com/h11/feedbox/items/23/occurrences/117235378113/
+// https://transform.tools/json-to-typescript
+type Occurrence = {
+    url: string
+    occurrence: {
+        feedurl: string
+        body?: {
+            message?: {
+                body?: string
+            }
+            trace_chain?: Array<{
+                exception?: {
+                    message?: string
+                }
+            }>
+        }
+    }
+}
+type RollbarPayload = {
+    event_name: 'occurrence'
+    data: Occurrence
+}
+
+// from worker environment
+declare const TELEGRAM_BOT_TOKEN: string
+declare const TELEGRAM_CHAT_ID: string
+
+// ---
 
 addEventListener('fetch', event => {
     event.respondWith(handle(event))
 })
 
-async function handle(event) {
+async function handle(event: FetchEvent) {
     const req = event.request
     if (req.method.toUpperCase() === 'POST') {
         try {
@@ -20,23 +49,19 @@ async function handle(event) {
     return response
 }
 
-async function dispatch(payload) {
+async function dispatch(payload: RollbarPayload) {
     const evt = payload.event_name
     if (evt === 'occurrence') {
         await handleOccurrence(payload.data)
     }
 }
 
-async function handleOccurrence(data) {
+async function handleOccurrence(data: Occurrence) {
     const url = data.url
     const feedurl = data.occurrence.feedurl
-    const exception = (() => {
-        try {
-            return data.occurrence.body.trace_chain[0].exception.message
-        } catch (err) {
-            return err.message
-        }
-    })()
+    const exception =
+        data.occurrence.body?.trace_chain?.[0]?.exception?.message ??
+        data.occurrence.body?.message?.body
     const text = [
         `rollbar = <a href="${url}">${url}</a>`,
         `feedurl = <a href="${feedurl}">${feedurl}</a>`,
@@ -45,7 +70,7 @@ async function handleOccurrence(data) {
     await sendToTelegram(text)
 }
 
-async function sendToTelegram(text) {
+async function sendToTelegram(text: string) {
     const url = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`
     const resp = await fetch(url, {
         method: 'POST',
