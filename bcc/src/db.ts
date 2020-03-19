6121752554c431f104b2a510f69677c0b267ce3e
h11 <nirisix@gmail.com>
2020-03-19 09:04:12 +0800

    bcc: simplify

--- bcc/src/db.ts
+++ bcc/src/db.ts
@@ -1,117 +1,81 @@
 import { execute } from './service/fauna'
 
-export const addTags = async (chat_id: number, tags: string[]) => {
-    /*
-    If(
-        Exists(Match(Index('bcc-get-tags-by-chat_id-sort_by-tag'), -1001450758329))
-        Update(
-            Select('ref',Get(Match(Index('bcc-get-tags-by-chat_id-sort_by-tag'), -1001450758329))),
-            {
-                data: {
-                    tags: Distinct(Union(
-                        Select(["data","tags"], Get(Match(Index('bcc-get-tags-by-chat_id-sort_by-tag'), -1001450758329))),
-                        ['#ocaml', '#gc', '#type']
-                    ))
-                }
-            }
-        ),
-        Create(
-            Collection('bcc'),
-            { data: { chat_id: -1001450758329, tags: ['#ocaml', '#gc', '#type'] } }
+/*
+{
+    name: "get_tags",
+    body: Query(
+        Lambda(
+            "chat_id",
+            If(
+                Exists(
+                    Match(Index("bcc-get-tags-by-chat_id-sort_by-tag"), Var("chat_id"))
+                ),
+                Select(
+                    ["data"],
+                    Paginate(
+                        Match(Index("bcc-get-tags-by-chat_id-sort_by-tag"), Var("chat_id")),
+                        100000
+                    )
+                ),
+                []
+            )
         )
     )
-    */
+}
+
+{
+    name: "add_tags",
+    body: Query(
+        Lambda(
+            ["chat_id", "tags"],
+            If(
+                Exists(
+                    Match(Index("bcc-get-tags-by-chat_id-sort_by-tag"), Var("chat_id"))
+                ),
+                Update(
+                    Select(
+                        "ref",
+                        Get(
+                            Match(Index("bcc-get-tags-by-chat_id-sort_by-tag"), Var("chat_id"))
+                        )
+                    ),
+                    {
+                        data: {
+                            tags: Distinct(
+                                Union(
+                                    Select(
+                                        ["data", "tags"],
+                                        Get(
+                                            Match(Index("bcc-get-tags-by-chat_id-sort_by-tag"), Var("chat_id"))
+                                        )
+                                    ),
+                                    Var("tags")
+                                )
+                            )
+                        }
+                    }
+                ),
+                Create(Collection("bcc"), {
+                    data: { chat_id: Var("chat_id"), tags: Var("tags") }
+                })
+            )
+        )
+    )
+}
+*/
+
+export const addTags = async (chat_id: number, tags: string[]) => {
     const stmt = JSON.stringify({
-        if: {
-            exists: {
-                match: { index: 'bcc-get-tags-by-chat_id-sort_by-tag' },
-                terms: chat_id,
-            },
-        },
-        then: {
-            update: {
-                select: 'ref',
-                from: {
-                    get: {
-                        match: { index: 'bcc-get-tags-by-chat_id-sort_by-tag' },
-                        terms: chat_id,
-                    },
-                },
-            },
-            params: {
-                object: {
-                    data: {
-                        object: {
-                            tags: {
-                                distinct: {
-                                    union: [
-                                        {
-                                            select: ['data', 'tags'],
-                                            from: {
-                                                get: {
-                                                    match: {
-                                                        index:
-                                                            'bcc-get-tags-by-chat_id-sort_by-tag',
-                                                    },
-                                                    terms: chat_id,
-                                                },
-                                            },
-                                        },
-                                        tags,
-                                    ],
-                                },
-                            },
-                        },
-                    },
-                },
-            },
-        },
-        else: {
-            create: { collection: 'bcc' },
-            params: {
-                object: {
-                    data: {
-                        object: {
-                            chat_id,
-                            tags,
-                        },
-                    },
-                },
-            },
-        },
+        call: { function: 'add_tags' },
+        arguments: [chat_id, tags],
     })
     await execute(stmt)
 }
 
 export const getTags = async (chat_id: number): Promise<string[]> => {
-    /*
-    If(
-        Exists(Match(Index('bcc-get-tags-by-chat_id-sort_by-tag'), -1001450758329)),
-        Select(
-            ["data"],
-            Paginate(Match(Index('bcc-get-tags-by-chat_id-sort_by-tag'), -1001450758329), {size:100000})
-        ),
-        []
-    )
-    */
     const stmt = JSON.stringify({
-        if: {
-            exists: {
-                match: { index: 'bcc-get-tags-by-chat_id-sort_by-tag' },
-                terms: chat_id,
-            },
-        },
-        then: {
-            select: ['data'],
-            from: {
-                paginate: {
-                    match: { index: 'bcc-get-tags-by-chat_id-sort_by-tag' },
-                    terms: chat_id,
-                },
-                size: 100000,
-            },
-        },
-        else: [],
+        call: { function: 'get_tags' },
+        arguments: chat_id,
     })
     const tags = await execute<string[]>(stmt)
     return tags
