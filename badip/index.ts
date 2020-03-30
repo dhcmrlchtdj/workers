41ce558396d63bc898a24fdb113b32365d6abe72
h11 <me@h11.io>
2020-03-30 23:32:51 +0800

    badip | send message

--- badip/index.ts
+++ badip/index.ts
@@ -1,16 +1,28 @@
 import {} from '@cloudflare/workers-types'
 import { log } from '../_common/sentry'
+import { sendMessage } from '../_common/telegram'
 import { WorkerRouter } from '../_common/router'
 import * as db from './db'
 
 declare const FAUNA_KEY: string
 declare const SENTRY_KEY: string
+declare const TELEGRAM_BOT_TOKEN: string
+declare const TELEGRAM_CHAT_ID: string
 
 const router = new WorkerRouter()
     .post('/badip/report', async (event) => {
         const req = event.request
         const { ip } = await req.json()
+
         await db.report(ip)
+
+        const tg = sendMessage(TELEGRAM_BOT_TOKEN, {
+            parse_mode: 'MarkdownV2',
+            chat_id: Number(TELEGRAM_CHAT_ID),
+            text: `BadIP Found: \`${ip}\`\nhttps://www.cip.cc/${ip}`,
+        })
+        event.waitUntil(tg)
+
         return new Response('created', { status: 201 })
     })
     .get('/badip/all', async (_event) => {
