6e4d75c5029151a16880215a5d6521ee927253d7
h11 <me@h11.io>
2020-03-30 22:18:00 +0800

    add badip

---
+++ badip/db.ts
@@ -0,0 +1,84 @@
+import { execute } from '../_common/fauna'
+
+/*
+
+// badip_add
+Query(Lambda(
+    ["ip"],
+    Select(
+        ["ts"],
+        Create(Collection("badip"), {
+            data: { ip: Var("ip"), timestamp: Now(null) }
+        })
+    )
+))
+
+// badip_get_all
+Query(Lambda(
+    [],
+    Select(
+        ["data"],
+        Distinct(Map(
+            Paginate(Match(Index("badip-ip")), 100000),
+            Lambda(["x"], Select(["data", "ip"], Get(Var("x"))))
+        ))
+    )
+))
+
+// badip_get_recent
+Query(Lambda(
+    ["days"],
+    Let(
+        {now: Now()},
+        Select(
+            ["data"],
+            Distinct(Map(
+                Filter(
+                    Paginate(Match(Index("badip-ip")), 100000),
+                    Lambda(
+                        ["x"],
+                        LTE(
+                            TimeDiff(
+                                Select(["data", "timestamp"], Get(Var("x"))),
+                                Var("now"),
+                                "days"
+                            ),
+                            Var("days")
+                        )
+                    )
+                ),
+                Lambda(["x"], Select(["data", "ip"], Get(Var("x"))))
+            ))
+        )
+    )
+))
+
+*/
+
+declare const FAUNA_KEY: string
+
+export const report = async (ip: string) => {
+    const stmt = JSON.stringify({
+        call: { function: 'badip_add' },
+        arguments: [ip],
+    })
+    await execute(FAUNA_KEY, stmt)
+}
+
+export const getAll = async (): Promise<string[]> => {
+    const stmt = JSON.stringify({
+        call: { function: 'badip_get_all' },
+        arguments: [],
+    })
+    const resp = await execute<string[]>(FAUNA_KEY, stmt)
+    return resp
+}
+
+export const getRecent = async (days: number): Promise<string[]> => {
+    const stmt = JSON.stringify({
+        call: { function: 'badip_get_recent' },
+        arguments: [days],
+    })
+    const resp = await execute<string[]>(FAUNA_KEY, stmt)
+    return resp
+}
---
+++ badip/index.ts
@@ -0,0 +1,44 @@
+import {} from '@cloudflare/workers-types'
+import { log } from '../_common/sentry'
+import { WorkerRouter } from '../_common/router'
+import * as db from './db'
+
+declare const FAUNA_KEY: string
+declare const SENTRY_KEY: string
+
+const router = new WorkerRouter()
+    .post('/badip/report', async (event) => {
+        const req = event.request
+        const { ip } = await req.json()
+        await db.report(ip)
+        return new Response('created', { status: 201 })
+    })
+    .get('/badip/all', async (_event) => {
+        const list = await db.getAll()
+        return new Response(JSON.stringify(list), {
+            status: 200,
+            headers: { 'content-type': 'application/json' },
+        })
+    })
+    .get('/badip/recent', async (_event) => {
+        const list = await db.getRecent(60)
+        return new Response(JSON.stringify(list), {
+            status: 200,
+            headers: { 'content-type': 'application/json' },
+        })
+    })
+
+const handle = async (event: FetchEvent) => {
+    try {
+        const resp = await router.route(event)
+        return resp
+    } catch (err) {
+        event.waitUntil(log(SENTRY_KEY, 'badip', event.request, err))
+        const msg = `${err}\n${err.stack}`
+        return new Response(msg, { status: 200 })
+    }
+}
+
+addEventListener('fetch', (event) => {
+    event.respondWith(handle(event))
+})
