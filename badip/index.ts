50f03777a5296ecd29dd44ab4186f462a22326f2
h11 <me@h11.io>
2020-04-02 03:05:25 +0800

    chore

--- badip/index.ts
+++ badip/index.ts
@@ -1,51 +1,11 @@
 import {} from '@cloudflare/workers-types'
 import { log } from '../_common/sentry'
-import { sendMessage } from '../_common/telegram'
 import { WorkerRouter } from '../_common/router'
 import { call } from '../_common/fauna'
 import { sortIP } from './sort-ip'
 
 declare const FAUNA_KEY: string
 declare const SENTRY_KEY: string
-declare const TELEGRAM_BOT_TOKEN: string
-declare const TELEGRAM_CHAT_ID: string
-
-const sendToIM = async (ip: string) => {
-    const text = [`BadIP Found: <code>${ip}</code>`]
-
-    // const ipReq = await fetch(`https://freegeoip.app/json/${ip}`)
-    // if (ipReq.ok) {
-    //     const ipData = await ipReq.json()
-    //     const loc = [ipData.country_name, ipData.region_name, ipData.city]
-    //         .filter(Boolean)
-    //         .join(', ')
-    //     text.push(loc)
-    // }
-
-    const ipReq = await fetch(`https://api.ip.sb/geoip/${ip}`)
-    if (ipReq.ok) {
-        const ipData = await ipReq.json()
-        const loc = [
-            ipData.country,
-            ipData.region,
-            ipData.city,
-            ipData.organization,
-        ]
-            .filter(Boolean)
-            .join(', ')
-        text.push(loc)
-        text.push(`https://ip.sb/ip/${ip}`)
-    }
-
-    text.push(`https://www.cip.cc/${ip}`)
-
-    await sendMessage(TELEGRAM_BOT_TOKEN, {
-        parse_mode: 'HTML',
-        chat_id: Number(TELEGRAM_CHAT_ID),
-        text: text.join('\n'),
-        disable_web_page_preview: true,
-    })
-}
 
 const router = new WorkerRouter()
     .post('/badip/report', async (event) => {
@@ -54,7 +14,6 @@ const router = new WorkerRouter()
         const ip = payload.ip
         if (ip) {
             await call('badip_add', ip)
-            // event.waitUntil(sendToIM(ip))
             return new Response('created', { status: 201 })
         } else {
             return new Response('invalid payload', { status: 200 })
