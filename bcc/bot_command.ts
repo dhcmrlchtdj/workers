d254e240de3770faaed04cd995279ca26ac7753f
h11 <me@h11.io>
2020-04-28 23:03:34 +0800

    bcc | whoami | add chat id

--- bcc/bot_command.ts
+++ bcc/bot_command.ts
@@ -9,10 +9,6 @@ const telegram = new TelegramClient(BCC_BOT_TOKEN)
 const fauna = new FaunaClient(FAUNA_KEY)
 const actions = new Map<string, (arg: string, msg: Message) => Promise<void>>()
 
-actions.set('/start', async (_arg: string, msg: Message) => {
-    await telegram.send('sendMessage', { chat_id: msg.chat.id, text: 'hello' })
-})
-
 actions.set('/add_tags', async (tags: string, msg: Message) => {
     await fauna.execute('bcc_add_tags', msg.chat.id, tags.split(' '))
 })
@@ -25,17 +21,20 @@ actions.set('/list', async (_arg: string, msg: Message) => {
 })
 
 actions.set('/whoami', async (_arg: string, msg: Message) => {
-    if (msg.forward_from) return
-    const user = msg.from
-    if (user) {
-        const chat_id = msg.chat.id
-        const text = JSON.stringify(user, null, 4)
-        await telegram.send('sendMessage', {
+    const chat_id = msg.chat.id
+    const text = JSON.stringify(
+        {
             chat_id,
-            text,
-            reply_to_message_id: msg.message_id,
-        })
-    }
+            ...(msg.from ?? {}),
+        },
+        null,
+        4,
+    )
+    await telegram.send('sendMessage', {
+        chat_id,
+        text,
+        reply_to_message_id: msg.message_id,
+    })
 })
 
 export const execute = async (cmd: string, arg: string, msg: Message) => {
