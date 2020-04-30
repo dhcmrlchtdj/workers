98fc55218b721a46902e0bf46167c0989fd886ca
h11 <me@h11.io>
2020-04-30 14:45:45 +0800

    sentry | verify signature

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ all:
 	$(MAKE) --no-print-directory build PROJ=badip
 	$(MAKE) --no-print-directory build PROJ=bcc
 	$(MAKE) --no-print-directory build PROJ=mzbot
-	$(MAKE) --no-print-directory build PROJ=rollbar
+	$(MAKE) --no-print-directory build PROJ=sentry
 
 build:
 	@rollup --format=es --input=$(PROJ)/index.ts --file=$(PROJ)/index.js --plugin=typescript --no-esModule
--- sentry/index.ts
+++ sentry/index.ts
@@ -2,6 +2,7 @@ import type {} from '@cloudflare/workers-types'
 import { TelegramClient } from '../_common/telegram'
 
 // from worker environment
+declare const SENTRY_HOOK_SECRET: string
 declare const TELEGRAM_BOT_TOKEN: string
 declare const MY_TELEGRAM_CHAT_ID: string
 
@@ -14,14 +15,19 @@ addEventListener('fetch', (event) => {
 async function handle(request: Request) {
     if (request.method.toUpperCase() === 'POST') {
         try {
-            const payload = await request.text()
-            const signature = request.headers.get('Sentry-Hook-Signature')
-            const fromSentry = await verifySignature(payload, signature)
+            const text = await request.text()
+            const sig = request.headers.get('Sentry-Hook-Signature')
+            const fromSentry = await verifySignature(SENTRY_HOOK_SECRET, text, sig)
             if (fromSentry) {
-                const body = JSON.parse(payload)
+                const body = JSON.parse(text)
                 await telegram.send('sendMessage', {
                     chat_id: Number(MY_TELEGRAM_CHAT_ID),
-                    text: body,
+                    text: JSON.stringify(body, null, 4),
+                })
+            } else {
+                await telegram.send('sendMessage', {
+                    chat_id: Number(MY_TELEGRAM_CHAT_ID),
+                    text,
                 })
             }
         } catch (_) {}
@@ -29,17 +35,31 @@ async function handle(request: Request) {
     return new Response(null, { status: 204 })
 }
 
+// https://docs.sentry.io/workflow/integrations/integration-platform/webhooks/?platform=node#verifying-the-signature
 async function verifySignature(
+    secret: string,
     message: string,
     signature: string | null,
 ): Promise<boolean> {
     if (signature === null) return false
-    const encoder = new TextEncoder()
-    const data = encoder.encode(message)
-    const hashBuffer = await crypto.subtle.digest('SHA-256', data)
-    const hashArray = Array.from(new Uint8Array(hashBuffer))
-    const hashHex = hashArray
-        .map((b) => b.toString(16).padStart(2, '0'))
-        .join('')
-    return hashHex === signature
+    const s = crypto.subtle
+    const enc = new TextEncoder()
+    const key = await s.importKey(
+        'raw',
+        enc.encode(secret),
+        {
+            name: 'HMAC',
+            hash: 'SHA-256',
+        },
+        false,
+        ['verify'],
+        // ['sign', 'verify'],
+    )
+    const sig = new Uint8Array(
+        signature.match(/[0-9a-fA-F]{2}/g)!.map((x) => parseInt(x, 16)),
+    )
+    return s.verify('HMAC', key, sig, enc.encode(message))
+
+    // const sig = await s.sign('HMAC', key, enc.encode(message))
+    // const hex = Array.from(new Uint8Array(sig)).map((b) => b.toString(16).padStart(2, '0')).join('')
 }
