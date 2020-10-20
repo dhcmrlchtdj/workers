dc8b232f65944b253482cd5a9d242175325c593f
h11 <me@h11.io>
2020-10-20 18:12:45 +0800

    add logplex

--- Makefile
+++ Makefile
@@ -4,6 +4,7 @@ PATH := ./node_modules/.bin:$(PATH)
 all:
 	$(MAKE) --no-print-directory build PROJ=bcc
 	$(MAKE) --no-print-directory build PROJ=rollbar
+	$(MAKE) --no-print-directory build PROJ=logplex
 
 build:
 	@rollup --format=es --input=$(PROJ)/index.ts --file=$(PROJ)/index.js --plugin=typescript --no-esModule
---
+++ logplex/index.ts
@@ -0,0 +1,67 @@
+import { encodeHtmlEntities, TelegramClient } from '../_common/telegram'
+
+// https://docs.rollbar.com/docs/webhooks
+
+// from worker environment
+declare const TELEGRAM_BOT_TOKEN: string
+declare const MY_TELEGRAM_CHAT_ID: string
+
+const telegram = new TelegramClient(TELEGRAM_BOT_TOKEN)
+
+// ---
+
+addEventListener('fetch', (event) => {
+    event.respondWith(handle(event.request))
+})
+
+async function handle(request: Request) {
+    if (request.method.toUpperCase() === 'POST') {
+        try {
+            const payload = await request.json()
+            await dispatch(payload)
+        } catch (_) {}
+    }
+    return new Response(null, { status: 204 })
+}
+
+async function dispatch(payload: RollbarPayload) {
+    const evt = payload.event_name
+    if (evt === 'occurrence') {
+        await handleOccurrence(payload.data)
+    }
+}
+
+async function handleOccurrence(data: Occurrence) {
+    const url = encodeHtmlEntities(data.url)
+    const error = encodeHtmlEntities(data.occurrence.title)
+    const text = `${url}\n<pre>${error}</pre>`
+    await telegram.send('sendMessage', {
+        parse_mode: 'HTML',
+        chat_id: Number(MY_TELEGRAM_CHAT_ID),
+        text,
+    })
+}
+
+// https://rollbar.com/h11/feedbox/items/23/occurrences/117235378113/
+// https://transform.tools/json-to-typescript
+type Occurrence = {
+    url: string
+    occurrence: {
+        title: string
+        // feedurl: string
+        // body?: {
+        //     message?: {
+        //         body?: string
+        //     }
+        //     trace_chain?: Array<{
+        //         exception?: {
+        //             message?: string
+        //         }
+        //     }>
+        // }
+    }
+}
+type RollbarPayload = {
+    event_name: 'occurrence'
+    data: Occurrence
+}
