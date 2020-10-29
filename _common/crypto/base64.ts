ef8d9e8407886ab41c589ccf978e9146f9df6b7b
h11 <me@h11.io>
2020-10-29 10:38:34 +0800

    fix base64

--- _common/crypto/base64.ts
+++ _common/crypto/base64.ts
@@ -1,23 +1,9 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/btoa
 
 export const decode = (base64: string): string => {
-    const decoded = atob(base64)
-    const u8 = new Uint8Array(decoded.length)
-    for (let i = 0; i < decoded.length; i++) {
-        u8[i] = decoded.charCodeAt(i)
-    }
-    const u16 = new Uint16Array(u8.buffer)
-    const original = String.fromCharCode(...u16)
-    return original
+    return decodeURIComponent(escape(atob(base64)))
 }
 
 export const encode = (str: string): string => {
-    const u16 = new Uint16Array(str.length)
-    for (let i = 0; i < str.length; i++) {
-        u16[i] = str.charCodeAt(i)
-    }
-    const u8 = new Uint8Array(u16.buffer)
-    const converted = String.fromCharCode(...u8)
-    const encoded = btoa(converted)
-    return encoded
+    return btoa(unescape(encodeURIComponent(str)))
 }
