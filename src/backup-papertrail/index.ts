847dfda3b22683197ab1ef5960c5647a867cbcde
h11 <me@h11.io>
2020-11-26 13:16:37 +0800

    fix(backup-papertrail): too many subrequest

--- src/backup-papertrail/index.ts
+++ src/backup-papertrail/index.ts
@@ -24,41 +24,17 @@ const b2 = new BackBlaze(BACKUP_B2_KEY_ID, BACKUP_B2_KEY, BACKUP_B2_REGION)
 async function backup(_event: ScheduledEvent): Promise<void> {
     const yestoday = new Date()
     yestoday.setUTCDate(yestoday.getUTCDate() - 1)
-    const prefix = format(yestoday, 'YYYY-MM-DD', true)
+    const date = format(yestoday, 'YYYY-MM-DD-hh', true)
 
-    const archives: archive[] = await GET(
-        'https://papertrailapp.com/api/v1/archives.json',
+    const file = await GET(
+        `https://papertrailapp.com/api/v1/archives/${date}/download`,
         { 'X-Papertrail-Token': BACKUP_PAPERTRAIL_TOKEN },
-    ).then((r) => r.json())
+    ).then((r) => r.arrayBuffer())
 
-    const tasks = archives
-        .filter((x) => x.filename.startsWith(prefix))
-        .map(async (x) => {
-            const file = await GET(x._links.download.href, {
-                'X-Papertrail-Token': BACKUP_PAPERTRAIL_TOKEN,
-            }).then((r) => r.arrayBuffer())
-            await b2.putObject(
-                BACKUP_B2_BUCKET,
-                x.filename,
-                file,
-                'application/gzip',
-            )
-        })
-    await Promise.all(tasks)
-}
-
-///
-
-type archive = {
-    start: string
-    end: string
-    start_formatted: string
-    duration_formatted: string
-    filename: string
-    filesize: string
-    _links: {
-        download: {
-            href: string
-        }
-    }
+    await b2.putObject(
+        BACKUP_B2_BUCKET,
+        `${date}.tsv.gz`,
+        file,
+        'application/gzip',
+    )
 }
