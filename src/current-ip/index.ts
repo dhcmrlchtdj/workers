9fde6d1b16e8c2cc62f1c87722e7120c0d4446a7
h11 <me@h11.io>
2022-01-25 20:07:38 +0800

    add BA to current-ip

--- src/current-ip/index.ts
+++ src/current-ip/index.ts
@@ -1,4 +1,5 @@
 import { Telegram } from "../_common/service/telegram"
+import { getBA } from "../_common/basic_auth"
 import { listenFetch } from "../_common/listen"
 
 declare const IP: KVNamespace
@@ -7,36 +8,35 @@ declare const IP: KVNamespace
 declare const ROLLBAR_KEY: string
 declare const ROLLBAR_TG_BOT_TOKEN: string
 declare const ROLLBAR_TG_CHAT_ID: string
+declare const IP_PASS_NUC: string
 
 listenFetch("current-ip", ROLLBAR_KEY, handle)
 
-const telegram = new Telegram(ROLLBAR_TG_BOT_TOKEN)
-
 async function handle(event: FetchEvent): Promise<Response> {
     const req = event.request
+
     const currIp = req.headers.get("CF-Connecting-IP")
     if (currIp === null) {
         throw new Error("CF-Connecting-IP is empty")
     }
 
-    const prevIp = await IP.get("current")
-    if (prevIp === null) {
-        saveCurrentIp(currIp)
-    } else {
-        if (prevIp !== currIp) {
-            saveCurrentIp(currIp)
-        }
+    const [user, pass] = getBA(req.headers.get("authorization"))
+    if (user === "nuc" && pass === IP_PASS_NUC) {
+        await saveCurrentIp("nuc", currIp)
     }
 
     return new Response("ok")
 }
 
-async function saveCurrentIp(ip: string) {
-    await IP.put("current", ip)
-
-    await telegram.send("sendMessage", {
-        parse_mode: "HTML",
-        chat_id: Number(ROLLBAR_TG_CHAT_ID),
-        text: `IP changed to ${ip}`,
-    })
+async function saveCurrentIp(machine: string, currIp: string) {
+    const prevIp = await IP.get(machine)
+    if (prevIp !== currIp) {
+        await IP.put("current", currIp)
+        const telegram = new Telegram(ROLLBAR_TG_BOT_TOKEN)
+        await telegram.send("sendMessage", {
+            parse_mode: "HTML",
+            chat_id: Number(ROLLBAR_TG_CHAT_ID),
+            text: `IP changed to ${currIp}`,
+        })
+    }
 }
