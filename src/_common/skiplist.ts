c52ade5c3cdd6841dd16ec1603879514bc39749f
h11 <me@h11.io>
2022-11-04 20:00:24 +0800

    add skiplist

---
+++ src/_common/skiplist.ts
@@ -0,0 +1,132 @@
+const SKIPLIST_MAXLEVEL = 16
+const SKIPLIST_P = 0.5
+
+function randomLevel(): number {
+	let level = 1
+	while (Math.random() < SKIPLIST_P && level < SKIPLIST_MAXLEVEL) {
+		level++
+	}
+	return level
+}
+
+// eslint-disable-next-line
+export interface SkipItem {
+	lt(x: SkipItem): boolean
+	eq(x: SkipItem): boolean
+}
+
+class SkipNode<I extends SkipItem> {
+	item: I | undefined
+	forward: (SkipNode<I> | undefined)[]
+	constructor(level: number, item: I | undefined) {
+		this.item = item
+		this.forward = new Array<SkipNode<I> | undefined>(level)
+	}
+	lt(item: I | undefined): boolean {
+		if (item === undefined || this.item === undefined) return false
+		return this.item.lt(item)
+	}
+	eq(item: I | undefined): boolean {
+		if (item === undefined || this.item === undefined) return false
+		return this.item.eq(item)
+	}
+}
+
+export class SkipList<I extends SkipItem> {
+	level: number
+	header: SkipNode<I>
+
+	constructor() {
+		this.level = 1
+		this.header = new SkipNode<I>(SKIPLIST_MAXLEVEL, undefined)
+	}
+
+	search(item: I): I | undefined {
+		let curr: SkipNode<I> = this.header
+		for (let i = this.level - 1; i >= 0; i--) {
+			while (curr.forward[i]?.lt(item)) {
+				curr = curr.forward[i]!
+			}
+		}
+
+		// when the loop ended, curr.item < item <= curr.forward[0].item
+		// so we move to curr.forward[0]
+		const node = curr.forward[0]
+		if (node?.eq(item)) {
+			return node.item
+		} else {
+			return undefined
+		}
+	}
+
+	/// true: inserted new item
+	/// false: replaced old item
+	insert(item: I): boolean {
+		const update = new Array<SkipNode<I>>(this.level)
+
+		let curr: SkipNode<I> = this.header
+		for (let i = this.level - 1; i >= 0; i--) {
+			while (curr.forward[i]?.lt(item)) {
+				curr = curr.forward[i]!
+			}
+			update[i] = curr
+		}
+
+		const node = curr.forward[0]
+		if (node?.eq(item)) {
+			node.item = item
+			return false
+		} else {
+			const newLevel = randomLevel()
+
+			if (newLevel > this.level) {
+				this.level = newLevel
+				for (let i = this.level; i < newLevel; i++) {
+					update[i] = this.header
+				}
+			}
+
+			const newNode = new SkipNode(newLevel, item)
+			for (let i = 0; i < newLevel; i++) {
+				newNode.forward[i] = update[i]!.forward[i]
+				update[i]!.forward[i] = newNode
+			}
+
+			return true
+		}
+	}
+
+	/// true: deleted old item
+	/// false: not found
+	delete(item: I): boolean {
+		const update = new Array<SkipNode<I>>(this.level)
+
+		let curr: SkipNode<I> = this.header
+		for (let i = this.level - 1; i >= 0; i--) {
+			while (curr.forward[i]?.lt(item)) {
+				curr = curr.forward[i]!
+			}
+			update[i] = curr
+		}
+
+		const node = curr.forward[0]
+		if (node?.eq(item)) {
+			for (let i = 0; i < update.length; i++) {
+				if (update[i]!.forward[i] === node) {
+					update[i]!.forward[i] = node.forward[i]!
+				} else {
+					break
+				}
+			}
+			while (
+				this.level > 1 &&
+				this.header.forward[this.level] === undefined
+			) {
+				this.level--
+			}
+			return true
+		} else {
+			return false
+		}
+	}
+}
