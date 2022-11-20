396f6a827ebb6ed000bc5a05e6f537872aa8a5fc
h11 <me@h11.io>
2022-11-20 15:17:52 +0800

    add unittest for arc

---
+++ test/_common/lru.ts
@@ -0,0 +1,59 @@
+import { ARC } from "../../src/_common/lru.js"
+
+describe("ARC", () => {
+	// https://github.com/hashicorp/golang-lru/blob/v2.0.1/arc_test.go
+	// MPL-2.0 license
+	test("adaptive", () => {
+		const arc = new ARC<number, number>(4)
+
+		arc.set(0, 0)
+		arc.set(1, 1)
+		arc.set(2, 2)
+		arc.set(3, 3)
+		// @ts-expect-error
+		expect(arc.recent.size()).toBe(4)
+
+		arc.get(0)
+		arc.get(1)
+		// @ts-expect-error
+		expect(arc.frequent.size()).toBe(2)
+
+		arc.set(4, 4)
+		// @ts-expect-error
+		expect(arc.recentEvicted.size()).toBe(1)
+
+		arc.set(2, 2)
+		// @ts-expect-error
+		expect(arc.recentEvicted.size()).toBe(1)
+		// @ts-expect-error
+		expect(arc.p).toBe(1)
+		// @ts-expect-error
+		expect(arc.frequent.size()).toBe(3)
+
+		arc.set(4, 4)
+		// @ts-expect-error
+		expect(arc.recent.size()).toBe(0)
+		// @ts-expect-error
+		expect(arc.frequent.size()).toBe(4)
+
+		arc.set(5, 5)
+		// @ts-expect-error
+		expect(arc.recent.size()).toBe(1)
+		// @ts-expect-error
+		expect(arc.frequent.size()).toBe(3)
+		// @ts-expect-error
+		expect(arc.frequentEvicted.size()).toBe(1)
+
+		arc.set(0, 0)
+		// @ts-expect-error
+		expect(arc.recent.size()).toBe(0)
+		// @ts-expect-error
+		expect(arc.frequent.size()).toBe(4)
+		// @ts-expect-error
+		expect(arc.recentEvicted.size()).toBe(2)
+		// @ts-expect-error
+		expect(arc.frequentEvicted.size()).toBe(0)
+		// @ts-expect-error
+		expect(arc.p).toBe(0)
+	})
+})
