762671a69a743bac3895dc7aae62e9693cce2f11
h11 <me@h11.io>
2022-11-20 13:23:15 +0800

    rewrite lru

--- src/_common/lru.ts
+++ src/_common/lru.ts
@@ -1,128 +1,117 @@
-class LinkedListNode<T> {
-	elem: T
-	prev: LinkedListNode<T>
-	next: LinkedListNode<T>
-	constructor(elem: T) {
-		this.elem = elem
-		// only head.prev and tail.next would be null
-		// other node will be inserted into the LinkedList immediately
-		this.prev = null!
-		this.next = null!
-	}
-	pop() {
-		const prev = this.prev
-		const next = this.next
-		prev.next = next
-		next.prev = prev
-	}
-	insert(prev: LinkedListNode<T>, next: LinkedListNode<T>) {
-		prev.next = this
-		next.prev = this
-		this.next = next
-		this.prev = prev
-	}
-}
-export class LinkedList<T> {
-	head: LinkedListNode<T>
-	tail: LinkedListNode<T>
-	constructor() {
-		this.head = new LinkedListNode(null as T)
-		this.tail = new LinkedListNode(null as T)
-		this.head.next = this.tail
-		this.tail.prev = this.head
-	}
-	addFirst(elem: T): LinkedListNode<T> {
-		const node = new LinkedListNode(elem)
-		node.insert(this.head, this.head.next)
-		return node
-	}
-	addLast(elem: T): LinkedListNode<T> {
-		const node = new LinkedListNode(elem)
-		node.insert(this.tail.prev, this.tail)
-		return node
-	}
-	removeFirst(): LinkedListNode<T> {
-		const node = this.head.next
-		if (node === this.tail) {
-			throw new Error("empty list")
-		} else {
-			node.pop()
-			return node
-		}
-	}
-	removeLast(): LinkedListNode<T> {
-		const node = this.tail.prev
-		if (node === this.head) {
-			throw new Error("empty list")
-		} else {
-			node.pop()
-			return node
-		}
-	}
-	getFirst(): LinkedListNode<T> {
-		const node = this.head.next
-		if (node === this.tail) {
-			throw new Error("empty list")
-		} else {
-			return node
-		}
-	}
-	getLast(): LinkedListNode<T> {
-		const node = this.tail.prev
-		if (node === this.head) {
-			throw new Error("empty list")
-		} else {
-			return node
-		}
-	}
-	moveToFirst(node: LinkedListNode<T>) {
-		node.pop()
-		node.insert(this.head, this.head.next)
-	}
-	moveToLast(node: LinkedListNode<T>) {
-		node.pop()
-		node.insert(this.tail.prev, this.tail)
-	}
-}
+import { Option, Some, None } from "./option.js"
 
-class KV<K, V> {
+class Entry<K, V> {
+	prev: Entry<K, V>
+	next: Entry<K, V>
 	key: K
 	value: V
 	constructor(key: K, value: V) {
 		this.key = key
 		this.value = value
+		this.prev = null!
+		this.next = null!
 	}
 }
+
+class LinkedList<K, V> {
+	head: Entry<K, V>
+	tail: Entry<K, V>
+	constructor() {
+		const sentinel = new Entry(null, null) as Entry<K, V>
+		sentinel.prev = sentinel
+		sentinel.next = sentinel
+		this.head = sentinel
+		this.tail = sentinel
+	}
+	addFirst(e: Entry<K, V>) {
+		this.insert(e, this.head, this.head.next)
+	}
+	removeLast(): Entry<K, V> {
+		const last = this.tail.prev
+		this.remove(last)
+		return last
+	}
+	moveToFirst(e: Entry<K, V>) {
+		this.remove(e)
+		this.insert(e, this.head, this.head.next)
+	}
+	remove(e: Entry<K, V>) {
+		e.prev.next = e.next
+		e.next.prev = e.prev
+	}
+	insert(e: Entry<K, V>, prev: Entry<K, V>, next: Entry<K, V>) {
+		e.prev = prev
+		prev.next = e
+		e.next = next
+		next.prev = e
+	}
+}
+
 export class LRU<K, V> {
 	private capacity: number
-	private map: Map<K, LinkedListNode<KV<K, V>>>
-	private list: LinkedList<KV<K, V>>
+	private map: Map<K, Entry<K, V>>
+	private list: LinkedList<K, V>
 	constructor(capacity: number) {
 		this.capacity = capacity
 		this.map = new Map()
 		this.list = new LinkedList()
 	}
-	get(key: K): V | null {
-		const item = this.map.get(key)
-		if (!item) {
-			return null
-		}
-		this.list.moveToFirst(item)
-		return item.elem.value
-	}
-	set(key: K, value: V) {
-		const item = this.map.get(key)
-		if (item) {
-			item.elem.value = value
-			this.list.moveToFirst(item)
+	get(key: K): Option<V> {
+		const e = this.map.get(key)
+		if (e === undefined) {
+			return None
 		} else {
-			const newElem = new KV(key, value)
-			const newNode = this.list.addFirst(newElem)
-			if (this.map.size >= this.capacity) {
-				const oldNode = this.list.removeLast()
-				this.map.delete(oldNode.elem.key)
-			}
-			this.map.set(key, newNode)
+			this.list.moveToFirst(e)
+			return Some(e.value)
 		}
+	}
+	// true, added
+	// false, updated
+	set(key: K, value: V): boolean {
+		const e = this.map.get(key)
+		if (e === undefined) {
+			const e = new Entry(key, value)
+			if (this.map.size >= this.capacity) {
+				// remove lastest
+				const old = this.list.removeLast()
+				this.map.delete(old.key)
+			}
+			this.list.addFirst(e)
+			this.map.set(key, e)
+			return true
+		} else {
+			e.value = value
+			this.list.moveToFirst(e)
+			return false
+		}
+	}
+	// true, removed
+	// false, not exist
+	remove(key: K): boolean {
+		const e = this.map.get(key)
+		if (e === undefined) {
+			return false
+		} else {
+			this.list.remove(e)
+			this.map.delete(key)
+			return true
+		}
+	}
+	has(key: K): boolean {
+		return this.map.has(key)
+	}
+	peek(key: K): Option<V> {
+		const e = this.map.get(key)
+		if (e === undefined) {
+			return None
+		} else {
+			return Some(e.value)
+		}
+	}
+	keys(): IterableIterator<K> {
+		return this.map.keys()
+	}
+	size(): number {
+		return this.map.size
 	}
 }
