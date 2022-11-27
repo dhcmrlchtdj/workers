0f49028af31dfb80d39be78d3fcc080e972aa1b2
h11 <me@h11.io>
2022-11-27 20:46:57 +0800

    skiplist: revert Math.clz32

--- src/_common/skiplist.ts
+++ src/_common/skiplist.ts
@@ -1,34 +1,16 @@
 import { Option, Some, None } from "./option.js"
 
-// const SKIPLIST_MAXLEVEL = 32
-// const SKIPLIST_P = 0.25
-// function randomLevel(): number {
-//     let level = 1
-//     while (Math.random() < SKIPLIST_P) {
-//         level++
-//     }
-//     return Math.min(SKIPLIST_MAXLEVEL, level)
-// }
-
-// const SKIPLIST_MAXLEVEL = 16
-// const SKIPLIST_P = 0.5
-// function randomLevel(): number {
-//     let level = 1
-//     let x = Math.floor(Math.random() * 0xffff) // [0x0, 0xffff)
-//     while ((x & 0x1) === 1) {
-//         x >>= 1
-//         level++
-//     }
-//     return level
-// }
-
+// https://ticki.github.io/blog/skip-lists-done-right/
+// https://graphics.stanford.edu/~seander/bithacks.html
 const SKIPLIST_MAXLEVEL = 16
 // const SKIPLIST_P = 0.5
 function randomLevel(): number {
-	// https://ticki.github.io/blog/skip-lists-done-right/
-	// https://graphics.stanford.edu/~seander/bithacks.html
-	const r = Math.ceil(Math.random() * 0xffff) // (0x0, 0xffff]
-	const level = Math.clz32(r) - 15 // [16, 32) - 15
+	let level = 1
+	let x = Math.floor(Math.random() * 0xffff) // [0x0, 0xffff)
+	while ((x & 0x1) === 1) {
+		x >>= 1
+		level++
+	}
 	return level
 }
 
