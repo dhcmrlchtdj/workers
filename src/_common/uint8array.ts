fe0575766a5ad0421ce5bec4fed1564b2194be91
h11 <me@h11.io>
2022-12-24 08:51:47 +0800

    chore: buf to/from hex

--- src/_common/uint8array.ts
+++ src/_common/uint8array.ts
@@ -7,17 +7,33 @@ export const fromStr = (s: string) => new TextEncoder().encode(s)
 export const toStr = (u: Uint8Array) => new TextDecoder().decode(u)
 
 export const fromHex = (hex: string) => {
-	if (hex.length % 2 !== 0 || /[^0-9a-fA-F]/.test(hex)) {
-		throw new Error("not a valid hex string")
+	const buf = new Uint8Array(hex.length >>> 1)
+	for (let i = 0, len = hex.length; i < len; i += 2) {
+		const parsed = parseInt(hex.slice(i, i + 2), 16)
+		buf[i] = parsed
+		i += 2
 	}
-	const arr = hex.match(/[0-9a-fA-F]{2}/g)!.map((x) => parseInt(x, 16))
-	return new Uint8Array(arr)
+	return buf
 }
 
-export const toHex = (u: Uint8Array) =>
-	Array.from(u)
-		.map((b) => b.toString(16).padStart(2, "0"))
-		.join("")
+const u8ToHex = (() => {
+	const alphabet = "0123456789abcdef"
+	const table = new Array<string>(256)
+	for (let i = 0; i < 16; i++) {
+		const i16 = i * 16
+		for (let j = 0; j < 16; j++) {
+			table[i16 + j] = alphabet[i]! + alphabet[j]!
+		}
+	}
+	return table
+})()
+export const toHex = (u: Uint8Array) => {
+	let out = ""
+	for (let i = 0, len = u.length; i < len; i++) {
+		out += u8ToHex[u[i]!]
+	}
+	return out
+}
 
 export const concat = (bufs: Uint8Array[]): Uint8Array => {
 	const len = bufs.reduce((acc, b) => acc + b.length, 0)
