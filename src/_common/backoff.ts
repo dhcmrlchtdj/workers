e1cbb774ce4ae05063beb84d24b524f8fa73f798
h11 <me@h11.io>
2022-12-24 13:03:13 +0800

    fix: full jit backoff impl

--- src/_common/backoff.ts
+++ src/_common/backoff.ts
@@ -1,26 +1,26 @@
 // https://aws.amazon.com/blogs/architecture/exponential-backoff-and-jitter/
 
 // Usage:
-// const b = new Backoff(1000 * 5, 500)
+// const b = new Backoff(500, 5000)
 // await b.sleep()
 
 export class Backoff {
-	private attempt: number
+	private attempts: number
+	private minDealy: number // millisecond
 	private maxDelay: number // millisecond
-	private initialValue: number // millisecond
 
-	constructor(maxDelay: number, initialValue: number) {
-		this.attempt = 0
-		this.maxDelay = maxDelay
-		this.initialValue = initialValue
+	constructor(minDealyInMS: number, maxDelayInMS: number) {
+		this.attempts = 0
+		this.minDealy = minDealyInMS
+		this.maxDelay = maxDelayInMS
 	}
 
 	async sleep() {
 		// full jitter
-		const delay =
-			Math.min(this.maxDelay, this.initialValue * 2 ** this.attempt) *
-			Math.random()
-		this.attempt++
-		await new Promise((r) => setTimeout(r, delay, undefined))
+		const tmp =
+			Math.min(this.minDealy * 2 ** this.attempts, this.maxDelay) / 2
+		const delay = tmp * Math.random() + tmp
+		this.attempts++
+		await new Promise((r) => setTimeout(r, delay))
 	}
 }
