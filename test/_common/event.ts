0e027c17a15cee292839d5981f4f589b5c271748
h11 <me@h11.io>
2023-01-16 19:55:35 +0800

    event: add tests

--- test/_common/event.ts
+++ test/_common/event.ts
@@ -1,7 +1,7 @@
-import { Channel, sync } from "../../src/_common/event.js"
+import { Channel, select, sync } from "../../src/_common/event.js"
 
 describe("Channel", () => {
-	test("block", async () => {
+	test("simple", async () => {
 		const ch = new Channel<number>()
 		await Promise.all([
 			(async () => {
@@ -13,5 +13,70 @@ describe("Channel", () => {
 				expect(r).toBe(10)
 			})(),
 		])
+	})
+})
+
+describe("Select", () => {
+	test("select channel 1", async () => {
+		const ch1 = new Channel<number>()
+		const ch2 = new Channel<string>()
+
+		const background = (async () => {
+			const r = await sync(ch1.receive())
+			expect(r).toBe(1)
+		})()
+
+		const r = await select<number>(
+			ch1.send(1).wrap(() => 1),
+			ch2.send("").wrap(() => 2),
+		)
+		expect(r).toBe(1)
+
+		await background
+	})
+
+	test("select channel 2", async () => {
+		const ch1 = new Channel<number>()
+
+		const background = (async () => {
+			const r = await sync(ch1.send(10))
+			expect(r).toBe(true)
+		})()
+
+		const r = await select(
+			ch1.receive().wrap((n) => {
+				expect(n).toBe(10)
+				return n
+			}),
+		)
+		expect(r).toBe(10)
+
+		await background
+	})
+
+	test("select channel 3", async () => {
+		const ch1 = new Channel<number>()
+
+		const background = (async () => {
+			const r1 = await sync(ch1.send(10))
+			expect(r1).toBe(true)
+
+			const r2 = await sync(ch1.send(20))
+			expect(r2).toBe(true)
+		})()
+
+		await select(
+			ch1.receive().wrap((data) => {
+				expect(data).toBe(10)
+			}),
+		)
+
+		await select(
+			ch1.receive().wrap((data) => {
+				expect(data).toBe(20)
+			}),
+		)
+
+		await background
 	})
 })
