43842db5748184e60b736d62acc7ef1c487fcf95
h11 <me@h11.io>
2023-02-05 22:41:01 +0800

    btree

--- src/_common/btree.ts
+++ src/_common/btree.ts
@@ -1,82 +1,258 @@
-import { Option, some, none } from "./option.js"
-
-class Entry<K, V> {
-	// children[i] <= keys[i]
-	keys: K[]
-	values: V[] // leaf
-	children: Entry<K, V>[] // non-leaf
+class Entry<T> {
+	// children[i].keys < keys[i] <= children[i+1].keys
+	keys: T[]
+	children: Entry<T>[] // non-leaf
 	constructor() {
 		this.keys = []
-		this.values = []
 		this.children = []
+	}
+	valueOf(): unknown {
+		if (this.isLeaf()) {
+			return this.keys
+		} else {
+			return {
+				keys: this.keys,
+				children: this.children.map((c) => c.valueOf()),
+			}
+		}
+	}
+	toString(): string {
+		return JSON.stringify(this.valueOf(), null, 4)
 	}
 	isLeaf(): boolean {
 		return this.children.length === 0
 	}
-	search(key: K): number {
-		return this._binarySearch(key)
-	}
-	_linearSearch(key: K): number {
+	search(key: T): number {
 		let i = 0
 		const len = this.keys.length
 		while (i < len) {
 			if (key <= this.keys[i]!) {
 				return i
+			} else {
+				i++
 			}
-			i++
 		}
 		return i
 	}
-	_binarySearch(key: K): number {
-		let left = 0
-		let right = this.keys.length
-		while (left < right) {
-			const m = (left + right) >>> 1
-			if (this.keys[m]! < key) {
-				left = m + 1
+	has(key: T): boolean {
+		const idx = this.search(key)
+		if (this.isLeaf()) {
+			return this.keys[idx] === key
+		} else {
+			if (this.children[idx]) {
+				return this.children[idx]!.has(key)
 			} else {
-				right = m
+				return false
 			}
 		}
-		return left
+	}
+	add(key: T, maxKeys: number): boolean {
+		const idx = this.search(key)
+		if (this.isLeaf()) {
+			if (this.keys[idx] === key) {
+				return false
+			} else {
+				this.keys.splice(idx, 0, key)
+				return true
+			}
+		} else {
+			if (this.maybeSplitChild(idx, maxKeys)) {
+				return this.add(key, maxKeys)
+			} else {
+				return this.children[idx]!.add(key, maxKeys)
+			}
+		}
+	}
+	remove(key: T, minKeys: number): boolean {
+		const idx = this.search(key)
+		if (this.isLeaf()) {
+			if (this.keys[idx] === key) {
+				this.keys.splice(idx, 1)
+				return true
+			} else {
+				return false
+			}
+		} else {
+			if (this.maybeGrowChild(idx, minKeys)) {
+				return this.remove(key, minKeys)
+			} else {
+				return this.children[idx]!.remove(key, minKeys)
+			}
+		}
+	}
+	///
+	split(idx: number): {
+		newKey: T
+		leftChild: Entry<T>
+		rightChild: Entry<T>
+	} {
+		const newKey = this.keys[idx]!
+		const leftChild = this as Entry<T>
+		const rightChild = new Entry<T>()
+
+		rightChild.keys = this.keys.slice(idx + 1)
+		rightChild.children = this.children.slice(idx + 1)
+		if (rightChild.isLeaf()) {
+			rightChild.keys.unshift(newKey)
+		}
+
+		leftChild.keys = this.keys.slice(0, idx)
+		leftChild.children = this.children.slice(0, idx + 1)
+
+		return { newKey, leftChild, rightChild }
+	}
+	// true, if splitted
+	private maybeSplitChild(idx: number, maxKeys: number): boolean {
+		if (this.children[idx]!.keys.length < maxKeys) {
+			return false
+		}
+		const node = this.children[idx]!
+		const { newKey, leftChild, rightChild } = node.split(
+			Math.floor(maxKeys / 2),
+		)
+		this.keys.splice(idx, 0, newKey)
+		this.children.splice(idx, 1, leftChild, rightChild)
+		return true
+	}
+	// true, if grew
+	private maybeGrowChild(idx: number, minKeys: number): boolean {
+		if (this.children[idx]!.keys.length > minKeys) {
+			return false
+		}
+		if (this.children[idx]!.isLeaf()) {
+			if (idx > 0 && this.children[idx - 1]!.keys.length > minKeys) {
+				// steal from left child
+				const left = this.children[idx - 1]!
+				const right = this.children[idx]!
+
+				const stolenKey = left.keys.pop()!
+				this.keys[idx] = stolenKey
+				right.keys.unshift(stolenKey)
+			} else if (
+				idx < this.keys.length &&
+				this.children[idx + 1]!.keys.length > minKeys
+			) {
+				// steal from right child
+				const left = this.children[idx]!
+				const right = this.children[idx + 1]!
+
+				const stolenKey = right.keys.shift()!
+				this.keys[idx] = right.keys[0]!
+				left.keys.push(stolenKey)
+			} else {
+				if (idx >= this.keys.length) {
+					// merge with left child
+					idx--
+				} else {
+					// merge with right child
+				}
+				const left = this.children[idx]!
+				const right = this.children[idx + 1]!
+
+				left.keys.push(...right.keys)
+				this.keys.splice(idx, 1)
+				this.children.splice(idx + 1, 1)
+			}
+		} else {
+			if (idx > 0 && this.children[idx - 1]!.keys.length > minKeys) {
+				// steal from left child
+				const left = this.children[idx - 1]!
+				const right = this.children[idx]!
+
+				// move to right
+				const currentKey = this.keys[idx]!
+				right.keys.unshift(currentKey)
+
+				// move to parent
+				const stolenKey = left.keys.pop()!
+				this.keys[idx] = stolenKey
+
+				// move to right
+				const stolenChild = left.children.pop()!
+				right.children.unshift(stolenChild)
+			} else if (
+				idx < this.keys.length &&
+				this.children[idx + 1]!.keys.length > minKeys
+			) {
+				// steal from right child
+				const left = this.children[idx]!
+				const right = this.children[idx + 1]!
+
+				// move to left
+				const currentKey = this.keys[idx]!
+				left.keys.push(currentKey)
+
+				// move to parent
+				const stolenKey = right.keys.shift()!
+				this.keys[idx] = stolenKey
+
+				// move to left
+				const stolenChild = right.children.shift()!
+				left.children.push(stolenChild)
+			} else {
+				if (idx >= this.keys.length) {
+					// merge with left child
+					idx--
+				} else {
+					// merge with right child
+				}
+				const left = this.children[idx]!
+				const right = this.children[idx + 1]!
+
+				// move to child
+				const currentKey = this.keys[idx]!
+				left.keys.push(currentKey)
+
+				// merge keys and children
+				left.keys.push(...right.keys)
+				left.children.push(...right.children)
+
+				// fix keys and chidlren
+				this.keys.splice(idx, 1)
+				this.children.splice(idx + 1, 1)
+			}
+		}
+		return true
 	}
 }
 
-export class BTree<K extends string | number, V> {
-	private count: number
-	private root: Entry<K, V> | undefined
-	constructor() {
-		this.count = 0
-		this.root = undefined
+export class BTree<T> {
+	private root: Entry<T>
+	private minKeys: number
+	private maxKeys: number
+	constructor(degree: number) {
+		this.root = new Entry()
+		this.minKeys = degree - 1
+		this.maxKeys = degree * 2 - 1
 	}
-	size() {
-		return this.count
+	toString() {
+		return JSON.stringify(this.root.valueOf(), null, 4)
 	}
-	clear() {
-		this.count = 0
-		this.root = undefined
+	// true, if existed
+	has(key: T): boolean {
+		return this.root.has(key)
 	}
-	get(key: K): Option<V> {
-		let node = this.root
-		while (node !== undefined) {
-			const idx = node.search(key)
-			if (node.isLeaf()) {
-				if (node.keys[idx] === key) {
-					return some(node.values[idx]!)
-				} else {
-					return none
-				}
-			} else {
-				node = node.children[idx]
-			}
+	// true, if added
+	// false, if existed
+	add(key: T): boolean {
+		if (this.root.keys.length >= this.maxKeys) {
+			const { newKey, leftChild, rightChild } = this.root.split(
+				Math.floor(this.maxKeys / 2),
+			)
+			this.root = new Entry()
+			this.root.keys = [newKey]
+			this.root.children = [leftChild, rightChild]
 		}
-		return none
+		return this.root.add(key, this.maxKeys)
 	}
-	// set(key: K, value: V): Option<V> {
-	//     return None
-	// }
-	// delete(key: K): Option<V> {
-	//     if (this.root === undefined) return None
-	//     return None
-	// }
+	// true, if removed
+	// false, if not existed
+	remove(key: T): boolean {
+		if (this.root.keys.length === 0) return false
+		const removed = this.root.remove(key, this.minKeys)
+		if (this.root.keys.length === 0 && this.root.children.length > 0) {
+			this.root = this.root.children[0]!
+		}
+		return removed
+	}
 }
