c320bab292f7963f06154146f009d7e1ba0fb669
h11 <me@h11.io>
2023-04-10 21:04:07 +0800

    remove http response body && minify

--- src/_common/http-response.ts
+++ src/_common/http-response.ts
@@ -85,32 +85,28 @@ type CookieOption = {
 
 ///
 
-export const HttpOk = (body: BodyInit = "OK") =>
-	new Response(body, { status: 200 })
-export const HttpCreated = (body: BodyInit = "Created") =>
+export const HttpOk = (body?: BodyInit) => new Response(body, { status: 200 })
+export const HttpCreated = (body?: BodyInit) =>
 	new Response(body, { status: 201 })
 
 // client error
 
-export const HttpBadRequest = (body: BodyInit = "Bad Request") =>
+export const HttpBadRequest = (body?: BodyInit) =>
 	new Response(body, { status: 400 })
-export const HttpUnauthorized = (
-	auth: string[],
-	body: BodyInit = "Unauthorized",
-) =>
+export const HttpUnauthorized = (auth: string[], body?: BodyInit) =>
 	new Response(body, {
 		status: 401,
 		headers: {
 			"WWW-Authenticate": auth.join(", "),
 		},
 	})
-export const HttpForbidden = (body: BodyInit = "Forbidden") =>
+export const HttpForbidden = (body?: BodyInit) =>
 	new Response(body, { status: 403 })
-export const HttpNotFound = (body: BodyInit = "Not Found") =>
+export const HttpNotFound = (body?: BodyInit) =>
 	new Response(body, { status: 404 })
 export const HttpMethodNotAllowed = (
 	allowedMethods: string[],
-	body: BodyInit = "Method Not Allowed",
+	body?: BodyInit,
 ) =>
 	new Response(body, {
 		status: 405,
@@ -118,12 +114,10 @@ export const HttpMethodNotAllowed = (
 			Allow: allowedMethods.join(", "),
 		},
 	})
-export const HttpUnsupportedMediaType = (
-	body: BodyInit = "Unsupported Media Type",
-) => new Response(body, { status: 415 })
+export const HttpUnsupportedMediaType = (body?: BodyInit) =>
+	new Response(body, { status: 415 })
 
 /// Server error
 
-export const HttpInternalServerError = (
-	body: BodyInit = "Internal Server Error",
-) => new Response(body, { status: 500 })
+export const HttpInternalServerError = (body?: BodyInit) =>
+	new Response(body, { status: 500 })
