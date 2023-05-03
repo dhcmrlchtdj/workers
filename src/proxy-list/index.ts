2c06752c8c93ee89fb2d05aecf38a40900963484
h11 <me@h11.io>
2023-05-03 19:58:07 +0800

    proxy list | use kv for storage

--- src/proxy-list/index.ts
+++ src/proxy-list/index.ts
@@ -1,10 +1,7 @@
 import { getBA } from "../_common/http/basic_auth.js"
 import { createWorker } from "../_common/listen.js"
 import * as R from "../_common/http/response.js"
-import {
-	HttpInternalServerError,
-	HttpUnauthorized,
-} from "../_common/http/status.js"
+import { HttpUnauthorized } from "../_common/http/status.js"
 import { MIME_TEXT } from "../_common/http/mime.js"
 import { toBase64 } from "../_common/base64.js"
 
@@ -13,7 +10,7 @@ type ENV = {
 	R2apac: R2Bucket
 }
 
-type KVItem = { password: string; proxies: string }
+type KVItem = { password: string; proxy: string[] }
 
 ///
 
@@ -24,10 +21,8 @@ const worker = createWorker("proxy-list", async (req: Request, env: ENV) => {
 		cacheTtl: 60 * 60, // 60min
 	})
 	if (item?.password === pass) {
-		const p = await env.R2apac.get("proxy.txt")
-		if (p === null) throw HttpInternalServerError("[r2] 404")
-		const text = await p.text()
-		const b64 = toBase64(text)
+		const proxy = item.proxy.join("\n")
+		const b64 = toBase64(proxy)
 		const resp = R.build(R.body(b64), R.contentType(MIME_TEXT))
 		return resp
 	} else {
