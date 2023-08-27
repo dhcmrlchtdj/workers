7a74fb3c9faafdb25a90991f221ef5ab58043618
h11 <git@h11.io>
2023-08-27 21:14:22 +0800

    chore

--- src/r2-share/index.ts
+++ src/r2-share/index.ts
@@ -11,84 +11,77 @@ type ENV = {
 
 ///
 
-const exportedHandler: ExportedHandler<ENV> = {
-	async fetch(req, env, ec) {
-		const router = new W.Router<ENV>()
-		router.use("*", W.sendErrorToTelegram("r2-share"), W.serverTiming())
-		router.head("/share/*", W.serveHeadWithGet())
-		router.get(
-			"/share/*",
-			W.cacheResponse(),
-			async ({ req, env, param }) => {
-				const filename = param.get("*")!
-				const end = W.addServerTiming("r2")
-				const object = await env.R2share.get(filename)
-				end()
-				if (object === null) return HttpNotFound()
+const router = new W.Router<ENV>()
+router.use("*", W.sendErrorToTelegram("r2-share"), W.serverTiming())
+router.head("/share/*", W.serveHeadWithGet())
+router.get("/share/*", W.cacheResponse(), async ({ req, env, param }) => {
+	const filename = param.get("*")!
+	const end = W.addServerTiming("r2")
+	const object = await env.R2share.get(filename)
+	end()
+	if (object === null) return HttpNotFound()
 
-				const reqEtag = req.headers.get("If-None-Match")
-				const resp = R.build(
-					reqEtag?.includes(object.httpEtag)
-						? R.status(304)
-						: R.body(object.body),
-					R.header("etag", object.httpEtag),
-					R.cacheControl(
-						"public, must-revalidate, s-maxage=86400, max-age=604800",
-					),
-					(b) => object.writeHttpMetadata(b.headers),
-				)
+	const reqEtag = req.headers.get("If-None-Match")
+	const resp = R.build(
+		reqEtag?.includes(object.httpEtag)
+			? R.status(304)
+			: R.body(object.body),
+		R.header("etag", object.httpEtag),
+		R.cacheControl(
+			"public, must-revalidate, s-maxage=86400, max-age=604800",
+		),
+		(b) => object.writeHttpMetadata(b.headers),
+	)
 
-				return resp
+	return resp
+})
+router.put(
+	"/share",
+	W.checkContentType(MIME_FORM_DATA),
+	W.basicAuth(async (user, pass, { env }) => {
+		const item = await env.BA.get<{ password: string }>("r2-share", {
+			type: "json",
+			cacheTtl: 60 * 60, // 60min
+		})
+		return user === "token" && item?.password === pass
+	}),
+	async ({ req, env }) => {
+		const body = await req.formData()
+		const file = body.get("file")
+		if (!(file instanceof File)) {
+			return HttpBadRequest("`file` is not a File")
+		}
+
+		const id = String(Math.random()).slice(2)
+		let objectKey = `${Date.now()}.${id}`
+		if (file.name) objectKey += "." + file.name
+
+		const content = await file.arrayBuffer()
+
+		const uploaded = await env.R2share.put(
+			encodeURIComponent(objectKey),
+			content,
+			{
+				httpMetadata: {
+					contentType: file.type ?? detectContentType(content),
+				},
+				customMetadata: {
+					via: "http-api",
+				},
 			},
 		)
-		router.put(
-			"/share",
-			W.checkContentType(MIME_FORM_DATA),
-			W.basicAuth(async (user, pass, { env }) => {
-				const item = await env.BA.get<{ password: string }>(
-					"r2-share",
-					{
-						type: "json",
-						cacheTtl: 60 * 60, // 60min
-					},
-				)
-				return user === "token" && item?.password === pass
-			}),
-			async ({ req, env }) => {
-				const body = await req.formData()
-				const file = body.get("file")
-				if (!(file instanceof File)) {
-					return HttpBadRequest("`file` is not a File")
-				}
 
-				const id = String(Math.random()).slice(2)
-				let objectKey = `${Date.now()}.${id}`
-				if (file.name) objectKey += "." + file.name
-
-				const content = await file.arrayBuffer()
-
-				const uploaded = await env.R2share.put(
-					encodeURIComponent(objectKey),
-					content,
-					{
-						httpMetadata: {
-							contentType:
-								file.type ?? detectContentType(content),
-						},
-						customMetadata: {
-							via: "http-api",
-						},
-					},
-				)
-
-				const resp = R.build(
-					R.status(201),
-					R.header("location", "/share/" + uploaded.key),
-				)
-				return resp
-			},
+		const resp = R.build(
+			R.status(201),
+			R.header("location", "/share/" + uploaded.key),
 		)
-		return router.handle(req, env, ec)
+		return resp
 	},
+)
+
+///
+
+const exportedHandler: ExportedHandler<ENV> = {
+	fetch: (req, env, ec) => router.handle(req, env, ec),
 }
 export default exportedHandler
